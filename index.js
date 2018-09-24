30a1fdca7291f7b647d1bdbc647579f90906fc25
nwilliams8 <33866591+nwilliams8@users.noreply.github.com>
2018-09-24 17:34:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 
 const express = require('express');
 const app = express();
+const courses = [
+  { id: 1, name: 'course 1'},
+  { id: 2, name: 'course 2'},
+  { id: 3, name: 'course 3'},
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+app.get('/api/courses/:id', (req,res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was'),
+  res.send(course);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
